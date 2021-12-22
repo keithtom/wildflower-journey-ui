f262739e6d0c58be3109f2a75935dbd27193a05d
Adrian Del Balso <a.delbalso@gmail.com>
2021-12-22 13:08:10 -0500

    adds global styles to storybook themeDecorator

--- .storybook/themeDecorator.js
+++ .storybook/themeDecorator.js
@@ -1,9 +1,12 @@
 import React from "react"
-import { theme } from "../styles/theme"
+import { GlobalStyle, theme } from "../styles/theme"
 import { ThemeProvider } from "styled-components"
 
 const ThemeDecorator = storyFn => (
-  <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    {storyFn()}
+  </ThemeProvider>
 )
 
 export default ThemeDecorator
