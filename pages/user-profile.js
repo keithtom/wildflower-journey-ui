76aaeef16048d6ad655b199ca77eaf1c3f0d78bb
Keith Tom <keith.tom@gmail.com>
2022-03-01 19:46:23 -0800

    WIP

--- pages/user-profile.js
+++ pages/user-profile.js
@@ -1,8 +1,17 @@
+import React, { useState, useEffect } from "react";
+
 import Head from 'next/head'
 import PageContainer from '@components/ui/PageContainer'
 import UserProfileContent from '@components/page-content/UserProfileContent'
 
 const UserProfilePage = ({}) => {
+  // API call gets user data, their school.
+  const [currentUser, setCurrentUser] = useState();
+
+  useEffect(() => {
+    // logged in user?
+    getCurrentUser({setCurrentUser: setCurrentUser});
+  }, []);
 
   return (
     <>
@@ -24,3 +33,20 @@ const UserProfilePage = ({}) => {
 }
 
 export default UserProfilePage
+
+function getCurrentUser() {
+
+}
+
+const user = {
+  name: 'Maya Walley',
+  role: 'Teacher Leader',
+  profileImage: 'https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
+  phoneNumber: '(917) 123-4567',
+  location: 'New York City',
+  skills: [
+    'Finance',
+    'Home Schooling',
+    'Real Estate'
+  ]
+}
