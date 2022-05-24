773d8b643c14d34f947056b77246a95ad9577444
Keith Tom <keith.tom@gmail.com>
2022-05-24 10:04:06 -0700

    Remove CORS credentials

--- api/search.js
+++ api/search.js
@@ -8,7 +8,6 @@ export default axios.create({
   mode: 'no-cors',
   headers: {
           'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          'Access-Control-Allow-Credentials': true,
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
           'Content-Type': 'application/json',
