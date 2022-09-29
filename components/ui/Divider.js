6b5201c9c692224f4174892ef4cc00c746a5f20a
Taylor Zanke <taylor.zanke@gmail.com>
2022-09-28 17:13:37 -0700

    build the ui for milestones to consider

--- components/ui/Divider.js
+++ components/ui/Divider.js
@@ -2,7 +2,7 @@ import { default as MaterialDivider } from "@mui/material/Divider";
 import { styled, css } from "@mui/material/styles";
 
 const CustomDivider = styled(MaterialDivider)`
-  border-bottom: 1px solid ${({ theme }) => theme.color.neutral.lightened};
+  border-bottom: 1px solid ${({ theme }) => theme.color.neutral.main};
 `;
 
 const Divider = ({ children, ...props }) => {
