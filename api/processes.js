19b2b51b52c537309172cce8aedeaf75cc8d877f
Taylor Zanke <taylor.zanke@gmail.com>
2023-01-06 14:55:07 -0800

    build the login, logout, forgot pass ui

--- api/processes.js
+++ api/processes.js
@@ -4,26 +4,23 @@ const api = axios.create({
   // baseURL: "http://localhost:3001/v1/workflow",
   baseURL: "https://api.wildflowerschools.org/v1/workflow",
   timeout: 3000,
-  mode: 'no-cors',
+  mode: "no-cors",
   headers: {
-          'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          'Content-Type': 'application/json',
+    "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers":
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+    "Content-Type": "application/json",
   },
 });
 
-async function index(){
+async function index() {}
 
-};
-
-async function complete(taskId){
+async function complete(taskId) {
   const response = await api.put(`/steps/${taskId}/complete`);
   // if response good, great.  else.  error out?
-};
+}
 
-async function uncomplete(){
+async function uncomplete() {}
 
-};
-
-export default {complete}
+export default { complete };
