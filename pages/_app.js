ef0e6143620c730c84208cdfb7d7c10d62648268
Taylor Zanke <taylor.zanke@gmail.com>
2023-01-23 11:32:57 -0800

    remove add wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -4,23 +4,12 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { UserProvider, useUserContext } from "../lib/useUserContext";
 
-function AppWrapper(props) {
-  const { currentUser, setCurrentUser } = useUserContext();
-  useEffect(() => {
-    props.currentUser && setCurrentUser(props.currentUser);
-  }, [currentUser]);
-
-  return <>{props.children}</>;
-}
-
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <UserProvider>
-        <AppWrapper {...pageProps}>
-          <Component {...pageProps} />
-        </AppWrapper>
+        <Component {...pageProps} />
       </UserProvider>
     </ThemeProvider>
   );
