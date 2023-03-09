6e7a83215446f9bae1c1d40a4b81d89752b06491
Taylor Zanke <taylor.zanke@gmail.com>
2023-03-09 14:41:40 -0800

    add nprogress package

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { theme } from "../styles/theme";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { UserProvider } from "../lib/useUserContext";
+import { useRouter } from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
 function MyApp({ Component, pageProps }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const Router = useRouter();
+  useEffect(() => {
+    NProgress.configure({ showSpinner: false, color: "#00A69C" });
+
+    Router.events.on("routeChangeStart", (url) => {
+      // setIsLoading(true);
+      NProgress.start();
+    });
+    Router.events.on("routeChangeComplete", (url) => {
+      // setIsLoading(false);
+      NProgress.done(false);
+    });
+
+    Router.events.on("routeChangeError", (url) => {
+      setIsLoading(false);
+    });
+  }, [Router]);
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
