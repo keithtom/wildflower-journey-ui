0cbae2ceeaf6d029c1b23f37cc54e9950435bc18
Taylor Zanke <taylor.zanke@gmail.com>
2023-03-29 14:18:04 -0700

    temporarily remove filepond plugin from onboarding and replace with native html input

--- pages/welcome/add-profile-info.js
+++ pages/welcome/add-profile-info.js
@@ -8,6 +8,7 @@ import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orien
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
 registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
+import { useRouter } from "next/router";
 
 import {
   Button,
@@ -44,8 +45,22 @@ const StyledFilePond = styled(FilePond)`
 
 const AddProfileInfo = ({}) => {
   const [profilePicture, setProfilePicture] = useState();
+  const router = useRouter();
+
+  const {
+    control,
+    register,
+    handleSubmit,
+
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    router.push("/ssj");
+    console.log(data);
+  };
 
   // console.log({ profilePicture });
+  // console.log({ errors });
 
   const isExistingTL = false;
 
@@ -56,88 +71,99 @@ const AddProfileInfo = ({}) => {
         <Grid container alignItems="center" justifyContent="center">
           <Grid item xs={12} sm={6} md={4} lg={3}>
             <Card>
-              <Stack spacing={6}>
-                <Grid container justifyContent="center">
-                  <Grid item>
-                    <Typography variant="h4" bold>
-                      Add a profile picture
-                    </Typography>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <Stack spacing={6}>
+                  <Grid container justifyContent="center">
+                    <Grid item>
+                      <Typography variant="h4" bold>
+                        Add a profile picture
+                      </Typography>
+                    </Grid>
                   </Grid>
-                </Grid>
-                {isExistingTL ? null : (
-                  <>
-                    <Card variant="primaryLightened" size="small">
-                      <Stack direction="row" spacing={3}>
-                        <Icon type="star" variant="primary" />
-                        <Typography variant="bodySmall">
-                          This helps other members of the Wildflower Schools
-                          network better connect with you!
-                        </Typography>
+                  {isExistingTL ? null : (
+                    <>
+                      <Card variant="primaryLightened" size="small">
+                        <Stack direction="row" spacing={3}>
+                          <Icon type="star" variant="primary" />
+                          <Typography variant="bodySmall">
+                            This helps other members of the Wildflower Schools
+                            network better connect with you!
+                          </Typography>
+                        </Stack>
+                      </Card>
+                      <Stack direction="row" spacing={3} alignItems="center">
+                        <Avatar
+                          size="sm"
+                          src="https://images.unsplash.com/photo-1589317621382-0cbef7ffcc4c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"
+                        />
+                        <Stack>
+                          <Typography variant="bodySmall" bold>
+                            Mary Truman
+                          </Typography>
+                          <Typography variant="bodySmall" lightened>
+                            Operations Guide
+                          </Typography>
+                        </Stack>
                       </Stack>
-                    </Card>
-                    <Stack direction="row" spacing={3} alignItems="center">
-                      <Avatar
-                        size="sm"
-                        src="https://images.unsplash.com/photo-1589317621382-0cbef7ffcc4c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"
+                    </>
+                  )}
+
+                  <Divider />
+
+                  <Grid container justifyContent="center">
+                    <Grid item xs={12} sm={8} md={6}>
+                      <Controller
+                        name="profilePicture"
+                        control={control}
+                        rules={{ required: true }}
+                        render={({ field }) => (
+                          <input type="file" {...field} />
+                          // <StyledFilePond
+                          //   type="file"
+                          //   files={profilePicture}
+                          //   allowReorder={false}
+                          //   allowMultiple={false}
+                          //   onupdatefiles={setProfilePicture}
+                          //   stylePanelAspectRatio="1:1"
+                          //   stylePanelLayout="circle"
+                          //   credits={false}
+                          //   labelIdle='Drag & Drop your profile picture or <span class="filepond--label-action">Browse</span>'
+                          //   {...field}
+                          // />
+                        )}
                       />
-                      <Stack>
-                        <Typography variant="bodySmall" bold>
-                          Mary Truman
-                        </Typography>
-                        <Typography variant="bodySmall" lightened>
-                          Operations Guide
-                        </Typography>
-                      </Stack>
-                    </Stack>
-                  </>
-                )}
-
-                <Divider />
-
-                <Grid container justifyContent="center">
-                  <Grid item xs={12} sm={8} md={6}>
-                    <StyledFilePond
-                      files={profilePicture}
-                      allowReorder={false}
-                      allowMultiple={false}
-                      onupdatefiles={setProfilePicture}
-                      stylePanelAspectRatio="1:1"
-                      stylePanelLayout="circle"
-                      credits={false}
-                      labelIdle='Drag & Drop your profile picture or <span class="filepond--label-action">Browse</span>'
-                    />
+                    </Grid>
                   </Grid>
-                </Grid>
-                <Divider />
-                <Grid container spacing={3} justifyContent="space-between">
-                  <Grid item xs={6}>
-                    <Link href="/welcome/confirm-demographic-info">
-                      <Button full variant="secondary">
-                        <Typography variant="bodyRegular">Back</Typography>
-                      </Button>
-                    </Link>
-                  </Grid>
-                  <Grid item xs={6}>
-                    {/* TODO: Change the destination depending on existing vs new TL */}
-                    <Link href="/ssj">
-                      <Button full disabled={!profilePicture}>
+                  <Divider />
+                  <Grid container spacing={3} justifyContent="space-between">
+                    <Grid item xs={6}>
+                      <Link href="/welcome/confirm-demographic-info">
+                        <Button full variant="secondary">
+                          <Typography variant="bodyRegular">Back</Typography>
+                        </Button>
+                      </Link>
+                    </Grid>
+                    <Grid item xs={6}>
+                      {/* TODO: Change the destination depending on existing vs new TL */}
+
+                      <Button full disabled={!profilePicture} type="submit">
                         <Typography variant="bodyRegular" light>
                           Confirm
                         </Typography>
                       </Button>
-                    </Link>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Link href="/ssj">
+                        <Button full variant="text">
+                          <Typography variant="bodyRegular" highlight>
+                            Skip for now
+                          </Typography>
+                        </Button>
+                      </Link>
+                    </Grid>
                   </Grid>
-                  <Grid item xs={12}>
-                    <Link href="/ssj">
-                      <Button full variant="text">
-                        <Typography variant="bodyRegular" highlight>
-                          Skip for now
-                        </Typography>
-                      </Button>
-                    </Link>
-                  </Grid>
-                </Grid>
-              </Stack>
+                </Stack>
+              </form>
             </Card>
           </Grid>
         </Grid>
