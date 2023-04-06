ac64c2bad7ed3fc2edb44e8bec0d2e2d62270c0c
Taylor Zanke <taylor.zanke@gmail.com>
2023-04-06 13:57:20 -0700

    Add real user data to onboarding, form validation improvements

--- pages/welcome/add-profile-info.js
+++ pages/welcome/add-profile-info.js
@@ -7,8 +7,12 @@ import "filepond/dist/filepond.min.css";
 import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
-import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
-registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview, FilePondPluginFileValidateSize);
+import FilePondPluginFileValidateSize from "filepond-plugin-file-validate-size";
+registerPlugin(
+  FilePondPluginImageExifOrientation,
+  FilePondPluginImagePreview,
+  FilePondPluginFileValidateSize
+);
 import { getCookie } from "cookies-next";
 import { FileChecksum } from "@lib/rails-filechecksum";
 import { useRouter } from "next/router";
@@ -35,15 +39,11 @@ import {
   MultiSelect,
   Radio,
   Divider,
+  PageContainer,
 } from "@ui";
 import Header from "@components/Header";
 import { PinDropSharp } from "@mui/icons-material";
 
-const PageContent = styled(Box)`
-  flex-grow: 1;
-  margin-top: ${({ theme }) => theme.util.appBarHeight}px;
-  padding: ${({ theme }) => theme.util.buffer * 6}px;
-`;
 const StyledFilePond = styled(FilePond)`
   .filepond--root {
     font-family: ${({ theme }) => theme.typography.family};
@@ -60,104 +60,127 @@ const AddProfileInfo = ({}) => {
   const { currentUser, setCurrentUser } = useUserContext();
 
   const handleSubmit = () => {
-    peopleApi.update(currentUser.id, { person: { 
-      profile_image: profileImage,  
-    }})
-    .then(response => {
-      if (response.error) {
-        console.error(error)
-      } else {
-        const personAttributes = response.data.attributes;
-        currentUser.attributes.imageUrl = `${baseUrl}${personAttributes.imageUrl}`;
-        setCurrentUser(currentUser);
-        router.push("/ssj");
-      }
-    })
+    peopleApi
+      .update(currentUser.id, {
+        person: {
+          profile_image: profileImage,
+        },
+      })
+      .then((response) => {
+        if (response.error) {
+          console.error(error);
+        } else {
+          const personAttributes = response.data.attributes;
+          currentUser.attributes.imageUrl = `${baseUrl}${personAttributes.imageUrl}`;
+          setCurrentUser(currentUser);
+          router.push("/ssj");
+        }
+      });
   };
 
   const isExistingTL = false;
+  const opsGuide = currentUser?.attributes.ssj.opsGuide.data.attributes;
 
   return (
-    <>
-      <Header user={false} />
-      <PageContent>
-        <Grid container alignItems="center" justifyContent="center">
-          <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Card>
-              <Stack spacing={6}>
-                <Grid container justifyContent="center">
-                  <Grid item>
-                    <Typography variant="h4" bold>
-                      Add a profile picture
-                    </Typography>
-                  </Grid>
+    <PageContainer isLoading={!currentUser} hideNav>
+      <Grid container alignItems="center" justifyContent="center">
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Card>
+            <Stack spacing={6}>
+              <Grid container justifyContent="center">
+                <Grid item>
+                  <Typography variant="h4" bold>
+                    Add a profile picture
+                  </Typography>
                 </Grid>
-                {isExistingTL ? null : (
-                  <>
-                    <Card variant="primaryLightened" size="small">
-                      <Stack direction="row" spacing={3}>
+              </Grid>
+              {isExistingTL ? null : (
+                <>
+                  <Card variant="primaryLightened" size="small">
+                    <Stack direction="row" spacing={3}>
+                      <Grid item>
                         <Icon type="star" variant="primary" />
-                        <Typography variant="bodySmall">
-                          This helps other members of the Wildflower Schools
-                          network better connect with you!
-                        </Typography>
-                      </Stack>
-                    </Card>
-                    <Stack direction="row" spacing={3} alignItems="center">
-                      <Avatar
-                        size="sm"
-                        src="https://images.unsplash.com/photo-1589317621382-0cbef7ffcc4c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"
-                      />
-                      <Stack>
-                        <Typography variant="bodySmall" bold>
-                          Mary Truman
-                        </Typography>
-                        <Typography variant="bodySmall" lightened>
-                          Operations Guide
-                        </Typography>
-                      </Stack>
+                      </Grid>
+                      <Typography variant="bodySmall">
+                        This helps other members of the Wildflower Schools
+                        network better connect with you!
+                      </Typography>
                     </Stack>
-                  </>
-                )}
+                  </Card>
+                  <Stack direction="row" spacing={3} alignItems="center">
+                    <Avatar size="sm" src={opsGuide?.imageUrl} />
+                    <Stack>
+                      <Typography variant="bodySmall" bold>
+                        {opsGuide?.firstName} {opsGuide?.lastName}
+                      </Typography>
+                      <Typography variant="bodySmall" lightened>
+                        Operations Guide
+                      </Typography>
+                    </Stack>
+                  </Stack>
+                </>
+              )}
 
-                <Divider />
+              <Divider />
 
-                <Grid container justifyContent="center">
-                  <Grid item xs={12} sm={8} md={6}>
-                    <StyledFilePond
-                      files={profilePicture}
-                      allowReorder={false}
-                      allowMultiple={false}
-                      maxFileSize="750KB"
-                      onupdatefiles={setProfilePicture}
-                      stylePanelAspectRatio="1:1"
-                      stylePanelLayout="circle"
-                      server={{
-                        process: (fieldName, file, metadata, load, error, progress, abort, transfer, options) => {
-                            // https://github.com/pqina/filepond/issues/279#issuecomment-479961967
-                            FileChecksum.create(file, (checksum_error, checksum) => {
-                                if (checksum_error) { 
-                                    console.error(checksum_error);
-                                    error();
-                                };
-                                axios.post(`${baseUrl}/rails/active_storage/direct_uploads`, {
-                                      blob: {
-                                          filename: file.name,
-                                          content_type: file.type,
-                                          byte_size: file.size,
-                                          checksum: checksum
-                                      }
-                                })
-                                .then((response) => {
-                                  if (!response.data) {
-                                    return error;
-                                  }
-                                  const signed_id = response.data.signed_id;
+              <Grid container justifyContent="center">
+                <Grid item xs={12} sm={8} md={6}>
+                  <StyledFilePond
+                    files={profilePicture}
+                    allowReorder={false}
+                    allowMultiple={false}
+                    maxFileSize="750KB"
+                    onupdatefiles={setProfilePicture}
+                    stylePanelAspectRatio="1:1"
+                    stylePanelLayout="circle"
+                    server={{
+                      process: (
+                        fieldName,
+                        file,
+                        metadata,
+                        load,
+                        error,
+                        progress,
+                        abort,
+                        transfer,
+                        options
+                      ) => {
+                        // https://github.com/pqina/filepond/issues/279#issuecomment-479961967
+                        FileChecksum.create(
+                          file,
+                          (checksum_error, checksum) => {
+                            if (checksum_error) {
+                              console.error(checksum_error);
+                              error();
+                            }
+                            axios
+                              .post(
+                                `${baseUrl}/rails/active_storage/direct_uploads`,
+                                {
+                                  blob: {
+                                    filename: file.name,
+                                    content_type: file.type,
+                                    byte_size: file.size,
+                                    checksum: checksum,
+                                  },
+                                }
+                              )
+                              .then((response) => {
+                                if (!response.data) {
+                                  return error;
+                                }
+                                const signed_id = response.data.signed_id;
 
-                                  axios.put(response.data.direct_upload.url, file, {
-                                    headers: response.data.direct_upload.headers,
+                                axios
+                                  .put(response.data.direct_upload.url, file, {
+                                    headers:
+                                      response.data.direct_upload.headers,
                                     onUploadProgress: (progressEvent) => {
-                                      progress(progressEvent.lengthComputable, progressEvent.loaded, progressEvent.total);
+                                      progress(
+                                        progressEvent.lengthComputable,
+                                        progressEvent.loaded,
+                                        progressEvent.total
+                                      );
                                     },
                                   })
                                   .then((response) => {
@@ -169,76 +192,76 @@ const AddProfileInfo = ({}) => {
                                       console.error(error);
                                       // error();
                                     }
-                                  })
-                                })
-                                .catch((error) => {
-                                  console.log(error);
-                                })
+                                  });
                               })
-                              return {
-                                abort: () => {
-                                    // This function is entered if the user has tapped the cancel button
-                                    // request.abort(); TODO: is there an active storage abort?
-                
-                                    // Let FilePond know the request has been cancelled
-                                    abort();
-                                },
-                            };
+                              .catch((error) => {
+                                console.log(error);
+                              });
+                          }
+                        );
+                        return {
+                          abort: () => {
+                            // This function is entered if the user has tapped the cancel button
+                            // request.abort(); TODO: is there an active storage abort?
+
+                            // Let FilePond know the request has been cancelled
+                            abort();
                           },
-                        headers: {
-                          Authorization: `Bearer ${token}`,
-                        },
-                        ondata: (formData) => {
-                          formData.append('blob', value);
-                          return formData;
-                        },
-                        onload: () => {
-                          props.onUploadComplete();
-                        },
-                      }}
-                      credits={false}
-                      labelIdle='Drag & Drop your profile picture or <span class="filepond--label-action">Browse</span>'
-                    />
-                  </Grid>
+                        };
+                      },
+                      headers: {
+                        Authorization: `Bearer ${token}`,
+                      },
+                      ondata: (formData) => {
+                        formData.append("blob", value);
+                        return formData;
+                      },
+                      onload: () => {
+                        props.onUploadComplete();
+                      },
+                    }}
+                    credits={false}
+                    labelIdle='Drag & Drop your profile picture or <span class="filepond--label-action">Browse</span>'
+                  />
                 </Grid>
-                <Divider />
-                <Grid container spacing={3} justifyContent="space-between">
-                  <Grid item xs={6}>
+              </Grid>
+              <Divider />
+              <Grid container spacing={3} justifyContent="space-between">
+                <Grid item xs={6}>
                   <Button
-                        full
-                        variant="secondary"
-                        disabled={!profilePicture}
-                        onClick={handleSubmit}
-                      >
-                        <Typography variant="bodyRegular">Back</Typography>
-                    </Button>
-                  </Grid>
-                  <Grid item xs={6}>
-                    {/* TODO: Change the destination depending on existing vs new TL */}
-                    <Button 
-                      full 
-                      disabled={!profilePicture}
-                      onClick={handleSubmit}
-                    >
-                      <Typography variant="bodyRegular" light>
-                        Confirm
-                      </Typography>
-                    </Button>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Button full variant="text" onClick={handleSubmit} >
-                      <Typography variant="bodyRegular" highlight>
-                        Skip for now
-                      </Typography>
-                    </Button>
-                  </Grid>
+                    full
+                    variant="secondary"
+                    disabled={!profilePicture}
+                    onClick={handleSubmit}
+                  >
+                    <Typography variant="bodyRegular">Back</Typography>
+                  </Button>
                 </Grid>
-              </Stack>
-            </Card>
-          </Grid>
+                <Grid item xs={6}>
+                  {/* TODO: Change the destination depending on existing vs new TL */}
+                  <Button
+                    full
+                    disabled={!profilePicture}
+                    onClick={handleSubmit}
+                  >
+                    <Typography variant="bodyRegular" light>
+                      Confirm
+                    </Typography>
+                  </Button>
+                </Grid>
+                <Grid item xs={12}>
+                  <Button full variant="text" onClick={handleSubmit}>
+                    <Typography variant="bodyRegular" highlight>
+                      Skip for now
+                    </Typography>
+                  </Button>
+                </Grid>
+              </Grid>
+            </Stack>
+          </Card>
         </Grid>
-      </PageContent>
-    </>
+      </Grid>
+    </PageContainer>
   );
 };
 
--- pages/welcome/create-password.js
+++ pages/welcome/create-password.js
@@ -3,7 +3,7 @@ import { styled, css } from "@mui/material/styles";
 import { useForm, Controller } from "react-hook-form";
 import { FormControlLabel, FormGroup, FormHelperText } from "@mui/material";
 import { useRouter } from "next/router";
-import registrationsApi from "../../api/registrations"
+import registrationsApi from "../../api/registrations";
 
 import {
   Button,
@@ -18,15 +18,9 @@ import {
   IconButton,
   TextField,
   Checkbox,
+  PageContainer,
 } from "@ui";
-import Header from "@components/Header";
-import { Router } from "@mui/icons-material";
 
-const PageContent = styled(Box)`
-  flex-grow: 1;
-  margin-top: ${({ theme }) => theme.util.appBarHeight}px;
-  padding: ${({ theme }) => theme.util.buffer * 6}px;
-`;
 const CreatePassword = ({}) => {
   const router = useRouter();
 
@@ -44,117 +38,115 @@ const CreatePassword = ({}) => {
   password.current = watch("password", "");
 
   const onSubmit = (data) => {
-    registrationsApi.setPassword(data.password, data.confirmPassword)
-      .then(response => {
+    registrationsApi
+      .setPassword(data.password, data.confirmPassword)
+      .then((response) => {
         router.push("/welcome/confirm-your-details");
       })
       .catch((error) => {
         console.error(error.message);
-      })
+      });
   };
 
   return (
-    <>
-      <Header user={false} />
-      <PageContent>
-        <Grid container alignItems="center" justifyContent="center">
-          <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Card>
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <Stack spacing={6}>
-                  <Grid container justifyContent="center">
-                    <Grid item>
-                      <Typography variant="h4" bold>
-                        Add a password
-                      </Typography>
-                    </Grid>
-                  </Grid>
-
-                  <Stack spacing={3}>
-                    <Controller
-                      name="password"
-                      control={control}
-                      rules={{ required: true }}
-                      render={({ field }) => (
-                        <TextField
-                          label="Password"
-                          type="password"
-                          placeholder="Your secure password"
-                          error={errors.password}
-                          helperText={
-                            errors &&
-                            errors.password &&
-                            errors.password &&
-                            "This field is required"
-                          }
-                          {...field}
-                        />
-                      )}
-                    />
-                    <Controller
-                      name="confirmPassword"
-                      control={control}
-                      rules={{
-                        required: true,
-                        validate: (value) => value === password.current,
-                      }}
-                      render={({ field }) => (
-                        <TextField
-                          label="Confirm password"
-                          type="password"
-                          placeholder="Confirm your password"
-                          error={errors.confirmPassword}
-                          helperText={
-                            errors &&
-                            errors.confirmPassword &&
-                            errors.confirmPassword.type === "required"
-                              ? "This field is required"
-                              : errors &&
-                                errors.confirmPassword &&
-                                errors.confirmPassword.type === "validate" &&
-                                "Your passwords do not match"
-                          }
-                          {...field}
-                        />
-                      )}
-                    />
-                    <Controller
-                      name="agreeTOS"
-                      control={control}
-                      rules={{
-                        required: true,
-                      }}
-                      render={({ field: { onChange, value } }) => (
-                        <FormGroup>
-                          <FormControlLabel
-                            value={value}
-                            control={<Checkbox error={errors.agreeTOS} />}
-                            label="I agree to the Terms of Service"
-                            onChange={onChange}
-                          />
-                          <FormHelperText error={errors.agreeTOS}>
-                            {errors &&
-                              errors.agreeTOS &&
-                              errors.agreeTOS.type === "required" &&
-                              "This field is required"}
-                          </FormHelperText>
-                        </FormGroup>
-                      )}
-                    />
-                  </Stack>
-
-                  <Button full disabled={isSubmitting} type="submit">
-                    <Typography variant="bodyRegular" light>
-                      Confirm
+    <PageContainer hideNav>
+      <Grid container alignItems="center" justifyContent="center">
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Card>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Stack spacing={6}>
+                <Grid container justifyContent="center">
+                  <Grid item>
+                    <Typography variant="h4" bold>
+                      Add a password
                     </Typography>
-                  </Button>
+                  </Grid>
+                </Grid>
+
+                <Stack spacing={3}>
+                  <Controller
+                    name="password"
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field }) => (
+                      <TextField
+                        label="Password"
+                        type="password"
+                        placeholder="Your secure password"
+                        error={errors.password}
+                        helperText={
+                          errors &&
+                          errors.password &&
+                          errors.password &&
+                          "This field is required"
+                        }
+                        {...field}
+                      />
+                    )}
+                  />
+                  <Controller
+                    name="confirmPassword"
+                    control={control}
+                    rules={{
+                      required: true,
+                      validate: (value) => value === password.current,
+                    }}
+                    render={({ field }) => (
+                      <TextField
+                        label="Confirm password"
+                        type="password"
+                        placeholder="Confirm your password"
+                        error={errors.confirmPassword}
+                        helperText={
+                          errors &&
+                          errors.confirmPassword &&
+                          errors.confirmPassword.type === "required"
+                            ? "This field is required"
+                            : errors &&
+                              errors.confirmPassword &&
+                              errors.confirmPassword.type === "validate" &&
+                              "Your passwords do not match"
+                        }
+                        {...field}
+                      />
+                    )}
+                  />
+                  <Controller
+                    name="agreeTOS"
+                    control={control}
+                    rules={{
+                      required: true,
+                    }}
+                    render={({ field: { onChange, value } }) => (
+                      <FormGroup>
+                        <FormControlLabel
+                          value={value}
+                          control={<Checkbox error={errors.agreeTOS} />}
+                          label="I agree to the Terms of Service"
+                          onChange={onChange}
+                        />
+                        <FormHelperText error={errors.agreeTOS}>
+                          {errors &&
+                            errors.agreeTOS &&
+                            errors.agreeTOS.type === "required" &&
+                            "This field is required"}
+                        </FormHelperText>
+                      </FormGroup>
+                    )}
+                  />
                 </Stack>
-              </form>
-            </Card>
-          </Grid>
+
+                <Button full disabled={isSubmitting} type="submit">
+                  <Typography variant="bodyRegular" light>
+                    Confirm
+                  </Typography>
+                </Button>
+              </Stack>
+            </form>
+          </Card>
         </Grid>
-      </PageContent>
-    </>
+      </Grid>
+    </PageContainer>
   );
 };
 
