d5479cead8a3b100cc785dc368eaddea81731d7d
Keith Tom <keith.tom@gmail.com>
2023-05-26 15:50:39 -0700

    add schools api

--- api/schools.js
+++ api/schools.js
@@ -1,3 +1,15 @@
-// index
-// show
-// update
+import wildflowerApi from "@api/base";
+
+const schoolsApi = wildflowerApi.register("/v1/schools", {});
+
+async function index() {
+  return schoolsApi.get();
+}
+
+async function show(id, config = {}) {
+  return workflowsApi.get(`/${id}`, config).then((response) => {
+    wildflowerApi.loadAllRelationshipsFromIncluded(response.data);
+  });  
+}
+
+export default { index, show };
