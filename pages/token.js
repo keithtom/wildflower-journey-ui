520d731433a53928a4df0ee9176e1ce7330c7a8d
Taylor Zanke <taylor.zanke@gmail.com>
2023-12-12 13:17:22 -0800

    move redirects to token.js and remove the /welcome page

--- pages/token.js
+++ pages/token.js
@@ -1,12 +1,13 @@
 import authApi from "@api/auth";
 import { useEffect } from "react";
 import { useUserContext } from "../lib/useUserContext";
-import Router from "next/router";
+import { useRouter } from "next/router";
 
 const Token = ({ query }) => {
-  const { currentUser, setCurrentUser } = useUserContext();
+  const { setCurrentUser } = useUserContext();
   const { token, redirect } = query;
   let loggedIn = false;
+  const router = useRouter();
 
   useEffect(() => {
     // Example link: https://platform.wildflowerschools.org/token?token=&redirect=https%3A%2F%2Fplatform.wildflowerschools.org%2Fwelcome%2Fexisting-tl
@@ -22,11 +23,66 @@ const Token = ({ query }) => {
             email: user.email,
             profileImage: user.imageUrl,
           });
-          Router.push(redirect);
+
+          //construct the relevant data to redirect based on
+          const personRoleList = result.data?.included?.find((a) => {
+            return a.id === personId;
+          })?.attributes?.roleList;
+          const personIsOnboarded = result.data?.included?.find((a) => {
+            return a.id === personId;
+          })?.attributes?.isOnboarded;
+          //extract individual roles to check with
+          const isEmergingTeacherLeader = personRoleList.includes(
+            "Emerging Teacher Leader"
+          );
+          const isTeacherLeader = personRoleList.includes("Teacher Leader");
+          const isOperationsGuide =
+            personRoleList.includes("Operations Guide") ||
+            personRoleList.includes("Ops Guide");
+
+          const isRegionalGrowthLead = personRoleList.includes(
+            "Regional Growth Lead"
+          );
+          const isFoundationPartner =
+            personRoleList.includes("Foundation Parnter");
+          const isCharterStaff = personRoleList.includes("Charter Staff");
+
+          //redirect to given routes based on role
+          switch (true) {
+            case personIsOnboarded && isEmergingTeacherLeader:
+              router.push("/ssj");
+              break;
+
+            case personIsOnboarded &&
+              (isTeacherLeader ||
+                isOperationsGuide ||
+                isRegionalGrowthLead ||
+                isFoundationPartner ||
+                isCharterStaff):
+              router.push("/network");
+              break;
+
+            case !personIsOnboarded && isEmergingTeacherLeader:
+              router.push("/welcome/new-etl");
+              break;
+
+            case !personIsOnboarded &&
+              (isTeacherLeader ||
+                isOperationsGuide ||
+                isRegionalGrowthLead ||
+                isFoundationPartner ||
+                isCharterStaff):
+              router.push("/welcome/existing-member");
+              break;
+
+            default:
+              router.push("/network");
+              break;
+          }
         })
         .catch((error) => {
           // if tokenAuth fails then
-          Router.push("/login");
+          router.push("/login");
           alert(error.message);
         });
     }
