2dfb209d2bed64f84f36d6e45466636655db133e
Li Ouyang <li.ouyang@gmail.com>
2024-01-03 13:14:16 -0500

    think we need this?

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
-module.exports = {
+import { withHighlightConfig } from "@highlight-run/next";
+
+module.exports = withHighlightConfig({
   reactStrictMode: true,
   env: {
     API_URL: process.env.API_URL,
     ENV: process.env.VERCEL_ENV,
     APP_ENV: process.env.APP_ENV,
-    NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID: process.env.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID,
+    NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID:
+      process.env.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID,
   },
-};
+});
