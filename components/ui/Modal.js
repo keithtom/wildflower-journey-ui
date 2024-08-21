3cc35224f11c523761c5e15bbf70a32b01bef6f4
Taylor Zanke <taylor.zanke@gmail.com>
2024-08-21 10:35:42 -0700

    Build the modal for editing personId fields

--- components/ui/Modal.js
+++ components/ui/Modal.js
@@ -1,95 +1,85 @@
-import { default as MaterialModal } from "@mui/material/Modal";
-import { Card, Grid, Icon, Stack, IconButton, Typography, Box } from "./index";
-import { styled, css } from "@mui/material/styles";
-
-const CustomModal = styled(MaterialModal)``;
-const ModalCard = styled(Card, {
-  shouldForwardProp: (prop) => prop !== "fixedActions",
-})`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 600px;
-  max-height: 640px;
-  overflow-y: scroll;
-  overflow-x: hidden;
-  max-width: 90%;
-  height: ${(props) => props.fixedHeight && "640px"};
-  ${(props) =>
-    props.fixedActions &&
-    css`
-      padding-bottom: 0;
-    `};
-`;
-const FixedActions = styled(Box)`
-  position: sticky;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background: ${({ theme }) => theme.color.neutral.lightest};
-  border-top: 1px solid ${({ theme }) => theme.color.neutral.main};
-  padding: ${({ theme }) => theme.util.buffer * 3}px;
-`;
-const FixedMenu = styled(Box)`
-  position: sticky;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  background: ${({ theme }) => theme.color.neutral.lightest};
-  border-top: 1px solid ${({ theme }) => theme.color.neutral.main};
-`;
+import {
+  Drawer,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+import { Grid, Icon, IconButton, Typography } from "./index";
 
 export default function Modal({
   title,
   toggle,
   children,
   noPadding,
+  fixedDrawer,
   fixedActions,
-  fixedMenu,
-  fixedHeight,
+
   ...props
 }) {
+  const drawerWidth = 200;
+  const dialogTitleHeight = 72;
+  const fixedActionsHeight = 48;
   return (
-    <CustomModal {...props} onClose={toggle}>
-      <ModalCard
-        elevated
-        noPadding
-        fixedActions={fixedActions}
-        fixedHeight={fixedHeight}
-      >
-        <Stack spacing={4}>
-          <Card noBorder noRadius sx={{ paddingBottom: 0 }}>
-            <Grid container alignItems="center" justifyContent="flex-end">
-              {title && (
-                <Grid item flex={1}>
-                  <Typography variant="h4" bold>
-                    {title}
-                  </Typography>
-                </Grid>
-              )}
-              <Grid item>
-                <IconButton size="small" onClick={toggle}>
-                  <Icon type="close" />
-                </IconButton>
-              </Grid>
-            </Grid>
-          </Card>
-          <Grid container>
-            {fixedMenu ? (
-              <Grid item xs={4} sx={{ borderRight: "1px solid #f1f1f1" }}>
-                <FixedMenu>{fixedMenu}</FixedMenu>
-              </Grid>
-            ) : null}
-            <Grid item flex={1}>
-              <Card noPadding={noPadding} noBorder noRadius>
-                {children}
-              </Card>
-            </Grid>
+    <Dialog
+      fullWidth="sm"
+      maxWidth="sm"
+      onClose={toggle}
+      {...props}
+      PaperProps={{ style: { height: fixedDrawer ? "600px" : "auto" } }}
+    >
+      <DialogTitle sx={{ zIndex: 1, height: `${dialogTitleHeight}px` }}>
+        <Grid container justifyContent="space-between" alignItems="center">
+          <Grid item>
+            <Typography variant="bodyLarge" bold>
+              {title}
+            </Typography>
           </Grid>
-        </Stack>
-        {fixedActions ? <FixedActions>{fixedActions}</FixedActions> : null}
-      </ModalCard>
-    </CustomModal>
+          <Grid item>
+            <IconButton onClick={toggle}>
+              <Icon type="close" />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </DialogTitle>
+
+      <DialogContent
+        dividers
+        sx={{
+          paddingLeft: fixedDrawer && `${drawerWidth + 24}px`,
+          zIndex: 0,
+        }}
+      >
+        {fixedDrawer ? (
+          <Drawer
+            variant="permanent"
+            anchor="left"
+            PaperProps={{
+              style: {
+                position: "absolute",
+                top: 72,
+                boxSizing: "border-box",
+                width: drawerWidth,
+                height: `calc(100% - ${dialogTitleHeight}px - ${
+                  fixedActions ? fixedActionsHeight : 0
+                }px - 1px )`,
+
+                borderLeft: "none",
+                borderBottom: "none",
+              },
+            }}
+          >
+            {fixedDrawer}
+          </Drawer>
+        ) : null}
+        <DialogContentText>{children}</DialogContentText>
+      </DialogContent>
+      {fixedActions ? (
+        <DialogActions sx={{ zIndex: 2, height: `${fixedActionsHeight}px` }}>
+          {fixedActions}
+        </DialogActions>
+      ) : null}
+    </Dialog>
   );
 }
