7c39f02b0c9071999c4c3fb7eb1a6fb7bf1f496f
Taylor Zanke <taylor.zanke@gmail.com>
2024-11-18 17:21:29 -0800

    translation on resources page

--- pages/ssj/[workflow]/resources.js
+++ pages/ssj/[workflow]/resources.js
@@ -3,6 +3,7 @@ import { getCookie } from "cookies-next";
 import ssjApi from "@api/ssj/ssj";
 import { useRouter } from "next/router";
 import Skeleton from "@mui/material/Skeleton";
+import { useTranslation } from "next-i18next";
 
 import useAuth from "@lib/utils/useAuth";
 import { PageContainer, Typography, Card, Stack, Icon, Grid, Chip } from "@ui";
@@ -12,12 +13,15 @@ import Resource from "@components/Resource";
 import Hero from "@components/Hero";
 import getAuthHeader from "@lib/getAuthHeader";
 import { clearLoggedInState, redirectLoginProps } from "@lib/handleLogout";
-
 import useSSJResources from "@hooks/useSSJResources";
+import TranslationToggle from "@components/TranslationToggle";
+import { getTranslatedAttr } from "@lib/utils/getTranslatedAttr";
 
 const Resources = () => {
   const router = useRouter();
   const { workflow } = router.query;
+
+  const { t } = useTranslation("common");
 
   const [showResourcesByCategory, setShowResourcesByCategory] = useState(true);
   const [showResourcesByPhase, setShowResourcesByPhase] = useState(false);
@@ -32,117 +36,136 @@ const Resources = () => {
   };
 
   const { resources, isLoading } = useSSJResources(workflow);
-  // console.log({ resources });
+  console.log({ resources });
 
   const hero = "/assets/images/ssj/wildflowerSystems.jpg";
 
   useAuth("/login");
 
   return (
-    <PageContainer>
-      <Stack spacing={12}>
-        <Hero imageUrl={hero} />
-        <Stack spacing={2}>
-          <Grid container justifyContent="space-between" alignItems="center">
-            <Grid item>
-              <Stack spacing={6} direction="row" alignItems="center">
-                <Icon type="fileBlank" variant="primary" size="large" />
-                <Typography variant="h3" bold>
-                  Resources
-                </Typography>
-              </Stack>
-            </Grid>
-            <Grid item>
-              <Stack spacing={2} direction="row" alignItems="center">
-                <Typography variant="bodyRegular" lightened>
-                  Group by
-                </Typography>
-                <Chip
-                  label="Category"
-                  variant={showResourcesByCategory && "primary"}
-                  onClick={handleShowResourcesByCategory}
-                />
-                <Chip
-                  label="Phase"
-                  variant={showResourcesByPhase && "primary"}
-                  onClick={handleShowResourcesByPhase}
-                />
-              </Stack>
-            </Grid>
-          </Grid>
-        </Stack>
-
-        {isLoading ? (
-          <Stack spacing={6}>
-            {Array.from({ length: 12 }, (_, i) => (
-              <Card key={i}>
-                <Stack spacing={6}>
-                  <Skeleton width={240} height={48} />
-                  <Stack spacing={3}>
-                    {Array.from({ length: 16 }, (_, j) => (
-                      <Skeleton key={j} height={64} m={0} variant="rounded" />
-                    ))}
-                  </Stack>
+    <>
+      <PageContainer>
+        <Stack spacing={12}>
+          <Hero imageUrl={hero} />
+          <Stack spacing={2}>
+            <Grid container justifyContent="space-between" alignItems="center">
+              <Grid item>
+                <Stack spacing={6} direction="row" alignItems="center">
+                  <Icon type="fileBlank" variant="primary" size="large" />
+                  <Typography variant="h3" bold>
+                    Resources
+                  </Typography>
                 </Stack>
-              </Card>
-            ))}
+              </Grid>
+              <Grid item>
+                <Stack spacing={2} direction="row" alignItems="center">
+                  <Typography variant="bodyRegular" lightened>
+                    {t("ssj_ui_content.group-by")}
+                  </Typography>
+                  <Chip
+                    label={t("ssj_ui_content.category")}
+                    variant={showResourcesByCategory && "primary"}
+                    onClick={handleShowResourcesByCategory}
+                  />
+                  <Chip
+                    label={t("ssj_ui_content.phase")}
+                    variant={showResourcesByPhase && "primary"}
+                    onClick={handleShowResourcesByPhase}
+                  />
+                </Stack>
+              </Grid>
+            </Grid>
           </Stack>
-        ) : showResourcesByCategory ? (
-          resources.by_category?.map((a, i) => {
-            const name = Object.keys(a)[0];
-            const array = Object.values(a);
-            return array[0]?.length ? (
-              <Card key={i}>
-                <Stack spacing={6}>
-                  <Stack direction="row" spacing={6} alignItems="center">
-                    <CategoryChip category={name} size="large" />
-                    <Typography variant="h4" lightened>
-                      {array[0].length}
-                    </Typography>
+
+          {isLoading ? (
+            <Stack spacing={6}>
+              {Array.from({ length: 12 }, (_, i) => (
+                <Card key={i}>
+                  <Stack spacing={6}>
+                    <Skeleton width={240} height={48} />
+                    <Stack spacing={3}>
+                      {Array.from({ length: 16 }, (_, j) => (
+                        <Skeleton key={j} height={64} m={0} variant="rounded" />
+                      ))}
+                    </Stack>
                   </Stack>
-                  <Stack spacing={3}>
-                    {array[0]?.map((r, i) => (
-                      <Resource
-                        title={r.data.attributes.title}
-                        link={r.data.attributes.link}
-                        key={i}
-                      />
-                    ))}
+                </Card>
+              ))}
+            </Stack>
+          ) : showResourcesByCategory ? (
+            resources.by_category?.map((a, i) => {
+              const name = Object.keys(a)[0];
+              const array = Object.values(a);
+              return array[0]?.length ? (
+                <Card key={i}>
+                  <Stack spacing={6}>
+                    <Stack direction="row" spacing={6} alignItems="center">
+                      <CategoryChip category={name} size="large" />
+                      <Typography variant="h4" lightened>
+                        {array[0].length}
+                      </Typography>
+                    </Stack>
+                    <Stack spacing={3}>
+                      {array[0]?.map((r, i) => (
+                        <Resource
+                          title={
+                            r.data.attributes[
+                              getTranslatedAttr(router.locale, "title")
+                            ] || r.data.attributes.title
+                          }
+                          link={r.data.attributes.link}
+                          key={i}
+                        />
+                      ))}
+                    </Stack>
                   </Stack>
-                </Stack>
-              </Card>
-            ) : null;
-          })
-        ) : (
-          resources.by_phase?.map((a, i) => {
-            const name = Object.keys(a)[0];
-            const array = Object.values(a);
-            return (
-              <Card key={i}>
-                <Stack spacing={6}>
-                  <Stack direction="row" spacing={6} alignItems="center">
-                    <PhaseChip phase={name} size="large" />
-                    <Typography variant="h4" lightened>
-                      {array[0].length}
-                    </Typography>
+                </Card>
+              ) : null;
+            })
+          ) : (
+            resources.by_phase?.map((a, i) => {
+              const name = Object.keys(a)[0];
+              const translatedName = t(`ssj_phases.${name}`);
+              const array = Object.values(a);
+              return (
+                <Card key={i}>
+                  <Stack spacing={6}>
+                    <Stack direction="row" spacing={6} alignItems="center">
+                      <PhaseChip phase={translatedName} size="large" />
+                      <Typography variant="h4" lightened>
+                        {array[0].length}
+                      </Typography>
+                    </Stack>
+                    <Stack spacing={3}>
+                      {array[0]?.map((r, i) => (
+                        <Resource
+                          title={r.data.attributes.title}
+                          link={r.data.attributes.link}
+                          key={i}
+                        />
+                      ))}
+                    </Stack>
                   </Stack>
-                  <Stack spacing={3}>
-                    {array[0]?.map((r, i) => (
-                      <Resource
-                        title={r.data.attributes.title}
-                        link={r.data.attributes.link}
-                        key={i}
-                      />
-                    ))}
-                  </Stack>
-                </Stack>
-              </Card>
-            );
-          })
-        )}
-      </Stack>
-    </PageContainer>
+                </Card>
+              );
+            })
+          )}
+        </Stack>
+      </PageContainer>
+      <TranslationToggle preferredLanguage={"en"} />
+    </>
   );
 };
 
 export default Resources;
+
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export async function getServerSideProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+      // Add any additional props you need to pass to the page component
+    },
+  };
+}
