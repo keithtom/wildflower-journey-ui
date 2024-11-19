e893f7dc816d9a973c69f766eb39e3a50c75c005
Taylor Zanke <taylor.zanke@gmail.com>
2024-11-19 11:31:55 -0800

    correection to configs

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,11 @@
+const path = require("path");
 module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"],
     fallbackLng: "en",
   },
+  localePath: path.resolve("./public/locales"),
   returnEmptyString: false, // Treat empty strings as missing keys
   returnNull: false, // Treat null as missing keys
 };
