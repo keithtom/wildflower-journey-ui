dabe32f067336ae8e225685a09fa3815da0dcf29
Taylor Zanke <taylor.zanke@gmail.com>
2024-12-06 08:08:21 -0800

    import the needed serverSideTranslations function from next-i18next on the ssj dash

--- pages/ssj/[workflow]/index.js
+++ pages/ssj/[workflow]/index.js
@@ -1309,3 +1309,14 @@ const waysToWorkTogether = [
     ],
   },
 ];
+
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export async function getServerSideProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+      // Add any additional props you need to pass to the page component
+    },
+  };
+}
