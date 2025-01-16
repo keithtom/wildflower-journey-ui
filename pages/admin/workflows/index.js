4ef7cf93fb6fab4ff0b22ef91544dced4687b368
Taylor Zanke <taylor.zanke@gmail.com>
2025-01-16 15:11:47 -0800

    open translations for two test schools and fix translatons not appearing for maggies user when in admin view

--- pages/admin/workflows/index.js
+++ pages/admin/workflows/index.js
@@ -240,3 +240,14 @@ const Workflows = ({}) => {
   );
 };
 export default Workflows;
+
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export async function getServerSideProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+      // Add any additional props you need to pass to the page component
+    },
+  };
+}
--- pages/admin/workflows/processes/[processId]/steps/[stepId].js
+++ pages/admin/workflows/processes/[processId]/steps/[stepId].js
@@ -963,3 +963,13 @@ const DecisionForm = ({ control, errors }) => {
     </Stack>
   );
 };
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export async function getServerSideProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+      // Add any additional props you need to pass to the page component
+    },
+  };
+}
