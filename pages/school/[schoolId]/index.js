a2b38270e600a883b9c9012bfa988a82b4720464
Taylor Zanke <taylor.zanke@gmail.com>
2025-02-18 14:51:19 -0800

    filter the ways to work together contents if the school is open

--- pages/school/[schoolId]/index.js
+++ pages/school/[schoolId]/index.js
@@ -33,7 +33,7 @@ const SchoolPage = () => {
 
   // console.log({ selectedWorkflow });
   // console.log({ currentUser });
-  console.log({ school });
+  // console.log({ school });
   // console.log({ progress });
   // console.log({ milestones });
   // console.log({ milestonesToDo });
@@ -107,101 +107,120 @@ const SchoolPage = () => {
       !member.attributes.schoolRoleList.includes("Emerging Teacher Leader")
   );
 
-  const waysToWorkTogether = [
-    {
-      name: t("ways_to_work_together.with_yourself"),
-      resources: [
-        {
-          title: t(
-            "ways_to_work_together.revisit_your_learning_and_growth_plan"
-          ),
-          url: "https://connected.wildflowerschools.org/posts/4432337-from-teacher-to-transformational-teacher-leader-recorded-etl-gathering?video_markers=learn%2Cgrowth%2Clearning+and+growth%2Clearning.%2Cgrowth%2C",
-          type: "Connected Post",
-          description: t(
-            "ways_to_work_together.revisit_your_learning_and_growth_plan_description"
-          ),
-        },
-        {
-          title: t(
-            "ways_to_work_together.learn_about_wildflower_ways_of_working"
-          ),
-          url: "https://connected.wildflowerschools.org/posts/4840229-self-management-learning-series-virtual-classroom-welcome",
-          type: "Connected Post",
-          description: t(
-            "ways_to_work_together.learn_about_wildflower_ways_of_working_description"
-          ),
-        },
-        {
-          title: t("ways_to_work_together.learn_about_liberatory_leadership"),
-          url: "https://connected.wildflowerschools.org/series/4588030-series-liberatory-leadership-series",
-          type: "Connected Series",
-          description: t(
-            "ways_to_work_together.learn_about_liberatory_leadership_description"
-          ),
-        },
-        {
-          title: t("ways_to_work_together.enroll_in_equity_training"),
-          url: "https://connected.wildflowerschools.org/series/4527958-series-equity-trainings",
-          type: "Connected Series",
-          description: t(
-            "ways_to_work_together.enroll_in_equity_training_description"
-          ),
-        },
-      ],
-    },
-    {
-      name: t("ways_to_work_together.with_your_team"),
-      resources: [
-        {
-          title: t("ways_to_work_together.identify_a_teacher_leader_partner"),
-          url: "https://docs.google.com/presentation/d/1ymc_PZDNMtAoNdIV0QHPWw5NdekQRQrdjhkT19eyivg/view",
-          type: "Google Slides",
-          description: t(
-            "ways_to_work_together.identify_a_teacher_leader_partner_description"
-          ),
-        },
-        {
-          title: t(
-            "ways_to_work_together.engage_a_growth_and_conciousness_coach"
-          ),
-          url: "https://connected.wildflowerschools.org/series/4406175-series-growth-connectedness-coaches",
-          type: "Connected Series",
-          description: t(
-            "ways_to_work_together.engage_a_growth_and_conciousness_coach_description"
-          ),
-        },
-        {
-          title: t("ways_to_work_together.engage_an_equity_or_abar_coach"),
-          url: "https://connected.wildflowerschools.org/series/4527903-series-equity-consultants",
-          type: "Connected Series",
-          description: t(
-            "ways_to_work_together.engage_an_equity_or_abar_coach_description"
-          ),
-        },
-      ],
-    },
-    {
-      name: t("ways_to_work_together.with_your_community"),
-      resources: [
-        {
-          title: t("ways_to_work_together.attend_wildflower_community_events"),
-          url: "https://connected.wildflowerschools.org/posts/4634392-wildflower-events-calendar",
-          type: "Connected Post",
-          description: t(
-            "ways_to_work_together.attend_wildflower_community_events_description"
-          ),
-        },
-        {
-          title: t("ways_to_work_together.learn_about_wildflower_school_pods"),
-          url: "https://connected.wildflowerschools.org/posts/4529540-essay-a-decentralized-network-by-erin-mckay",
-          type: "Connected Post",
-          description: t(
-            "ways_to_work_together.learn_about_wildflower_school_pods_description"
-          ),
-        },
-      ],
-    },
-  ];
+  // Filter resources based on school status
+  const waysToWorkTogether = useMemo(
+    () => [
+      {
+        name: t("ways_to_work_together.with_yourself"),
+        resources: [
+          {
+            title: t(
+              "ways_to_work_together.revisit_your_learning_and_growth_plan"
+            ),
+            url: "https://connected.wildflowerschools.org/posts/4432337-from-teacher-to-transformational-teacher-leader-recorded-etl-gathering?video_markers=learn%2Cgrowth%2Clearning+and+growth%2Clearning.%2Cgrowth%2C",
+            type: "Connected Post",
+            description: t(
+              "ways_to_work_together.revisit_your_learning_and_growth_plan_description"
+            ),
+            hideWhenOpen: true,
+          },
+          {
+            title: t(
+              "ways_to_work_together.learn_about_wildflower_ways_of_working"
+            ),
+            url: "https://connected.wildflowerschools.org/posts/4840229-self-management-learning-series-virtual-classroom-welcome",
+            type: "Connected Post",
+            description: t(
+              "ways_to_work_together.learn_about_wildflower_ways_of_working_description"
+            ),
+          },
+          {
+            title: t("ways_to_work_together.learn_about_liberatory_leadership"),
+            url: "https://connected.wildflowerschools.org/series/4588030-series-liberatory-leadership-series",
+            type: "Connected Series",
+            description: t(
+              "ways_to_work_together.learn_about_liberatory_leadership_description"
+            ),
+          },
+          {
+            title: t("ways_to_work_together.enroll_in_equity_training"),
+            url: "https://connected.wildflowerschools.org/series/4527958-series-equity-trainings",
+            type: "Connected Series",
+            description: t(
+              "ways_to_work_together.enroll_in_equity_training_description"
+            ),
+          },
+        ].filter((resource) => {
+          const isOpen = school?.data?.attributes?.status === "Open";
+          return !resource.hideWhenOpen || !isOpen;
+        }),
+      },
+      {
+        name: t("ways_to_work_together.with_your_team"),
+        resources: [
+          {
+            title: t("ways_to_work_together.identify_a_teacher_leader_partner"),
+            url: "https://docs.google.com/presentation/d/1ymc_PZDNMtAoNdIV0QHPWw5NdekQRQrdjhkT19eyivg/view",
+            type: "Google Slides",
+            description: t(
+              "ways_to_work_together.identify_a_teacher_leader_partner_description"
+            ),
+            hideWhenOpen: true,
+          },
+          {
+            title: t(
+              "ways_to_work_together.engage_a_growth_and_conciousness_coach"
+            ),
+            url: "https://connected.wildflowerschools.org/series/4406175-series-growth-connectedness-coaches",
+            type: "Connected Series",
+            description: t(
+              "ways_to_work_together.engage_a_growth_and_conciousness_coach_description"
+            ),
+          },
+          {
+            title: t("ways_to_work_together.engage_an_equity_or_abar_coach"),
+            url: "https://connected.wildflowerschools.org/series/4527903-series-equity-consultants",
+            type: "Connected Series",
+            description: t(
+              "ways_to_work_together.engage_an_equity_or_abar_coach_description"
+            ),
+          },
+        ].filter((resource) => {
+          const isOpen = school?.data?.attributes?.status === "Open";
+          return !resource.hideWhenOpen || !isOpen;
+        }),
+      },
+      {
+        name: t("ways_to_work_together.with_your_community"),
+        resources: [
+          {
+            title: t(
+              "ways_to_work_together.attend_wildflower_community_events"
+            ),
+            url: "https://connected.wildflowerschools.org/posts/4634392-wildflower-events-calendar",
+            type: "Connected Post",
+            description: t(
+              "ways_to_work_together.attend_wildflower_community_events_description"
+            ),
+          },
+          {
+            title: t(
+              "ways_to_work_together.learn_about_wildflower_school_pods"
+            ),
+            url: "https://connected.wildflowerschools.org/posts/4529540-essay-a-decentralized-network-by-erin-mckay",
+            type: "Connected Post",
+            description: t(
+              "ways_to_work_together.learn_about_wildflower_school_pods_description"
+            ),
+          },
+        ].filter((resource) => {
+          const isOpen = school?.data?.attributes?.status === "Open";
+          return !resource.hideWhenOpen || !isOpen;
+        }),
+      },
+    ],
+    [t, school?.data?.attributes?.status]
+  );
 
   return (
     <PageContainer title={school?.data.attributes.name}>
