4858f1f0e52bef57d6a872310d08f8ef7fad79d5
Taylor Zanke <taylor.zanke@gmail.com>
2025-02-25 09:23:38 -0800

    check if affiliated before showing link to personId

--- components/Nav.js
+++ components/Nav.js
@@ -198,6 +198,10 @@ const WorkflowNavItems = ({
   const isOpenSchoolChecklist = workflow.attributes.recurring === true;
   const isSchoolStartupJourney = workflow.attributes.recurring === false;
 
+  const isAffiliated = currentUser?.schools?.some(
+    (school) => school.affiliated === true
+  );
+
   if (isOpenSchoolChecklist) {
     return (
       <>
@@ -656,19 +660,23 @@ const Nav = ({ toggleNavOpen, navOpen }) => {
               }}
             >
               <NavList>
-                <NavListItemButton
-                  onClick={() =>
-                    router.push(`/network/people/${currentUser?.id}`)
-                  }
-                >
-                  <NavListItemIcon>
-                    <Icon type="user" variant="lightened" />
-                  </NavListItemIcon>
-                  <NavListItemText primary="Your Profile" />
-                </NavListItemButton>
-                <Divider
-                  sx={{ my: 2, borderColor: theme.color.neutral.lightened }}
-                />
+                {isAffiliated ? (
+                  <NavListItemButton
+                    onClick={() =>
+                      router.push(`/network/people/${currentUser?.id}`)
+                    }
+                  >
+                    <NavListItemIcon>
+                      <Icon type="user" variant="lightened" />
+                    </NavListItemIcon>
+                    <NavListItemText primary="Your Profile" />
+                  </NavListItemButton>
+                ) : null}
+                {isAffiliated ? (
+                  <Divider
+                    sx={{ my: 2, borderColor: theme.color.neutral.lightened }}
+                  />
+                ) : null}
                 {isAdmin ? (
                   <NavListItemButton
                     onClick={() => {
